db78511bcc61b16fe2aff2f658e1529700a37179
EXComm19 <excomm19@icloud.com>
2019-06-30 21:57:04 +1000

    change stuff

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
 const fs = require('fs')
+const bodyParser = require('body-parser')
 
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+// app.use( bodyParser.urlencoded({ extended: true }) ); // to support URL-encoded bodies
+//
 
 // app.post('/save', (request, response) => {
-//   console.log("server");
+//   console.log(request.body);
 //   const data = request.body
 //   console.log(data);
-//   // console.log("save");
-//   //
-//   // // fs.writeFile('/public/note.txt', data, (err) => {
-//   // //   if (err) throw err;
-//   // // })
-//   //
-//   response.json(data)
-//   // console.log(data);
-//
+//   fs.writeFile('./public/note/note.txt', data, (err) => {
+//     if (err) throw err;
+//   });
 // })
 
-app.post('/api', (request, response) => {
+app.post('/save', (request, response) => {
+  // console.log(request);
   const data = request.body;
-  response.json(data);
+  console.log(data);
+  response.json(request.body);
 });
--- public/note/note.js
+++ public/note/note.js
@@ -1,10 +1,34 @@
 async function save() {
-  note = document.getElementById('note').value
-
-  const data = {
-    note
+  var note = document.getElementById('note').value
+  var data = {
+    "note": note,
+    "save": "save"
   }
 
+  console.log(note);
+
+  // const options = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify(note)
+  // };
+  //
+  // // fetch('/save', options).then(response => {
+  // //   console.log(options);
+  // //   console.log("save");
+  // //   const res = response.json()
+  // //   console.log(res);
+  // // })
+  //
+  // const request = async () => {
+  //   const response = await fetch('/save', options);
+  //   const json = await response.json();
+  //   console.log(json);
+  // }
+  //
+  // request();
   const options = {
     method: 'POST',
     headers: {
@@ -12,8 +36,8 @@ async function save() {
     },
     body: JSON.stringify(data)
   };
-
-  const response = await fetch('/api', options);
+  const response = await fetch('/save', options);
   const json = await response.json();
   console.log(json);
+
 }
