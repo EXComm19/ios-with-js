ae54ec7d0c47bbb1eb2375baa62b03153e34c20f
EXComm19 <excomm19@icloud.com>
2019-06-30 16:52:17 +1000

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const fs = require('fs')
 
-app.listen(3000, () => console.log('listening at 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
 
 app.get('/save', (request, response) => {
