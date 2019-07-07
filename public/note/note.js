4687b57a21bdc69336d3a865b452354da9f8e2d2
EXComm19 <excomm19@icloud.com>
2019-07-07 17:35:23 +1000

    Make sperate notes for individual browser

--- public/note/note.js
+++ public/note/note.js
@@ -1,22 +1,20 @@
 window.onload = function () {
-  $.get("note.txt", function (data) {
-    document.getElementById('note').value = data;
-  });
+  document.getElementById('note').value = localStorage.getItem("note")
 
   document.getElementById('note').onkeyup = function () {
-    var note = document.getElementById('note').value
-    var data = {
-      "note": note,
-      "status": "saved"
-    }
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-    fetch('/api', options)
+    const note = document.getElementById('note').value
+    // const data = {
+    //   "note": note,
+    //   "status": "saved"
+    // }
+    localStorage.setItem("note", note)
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(data)
+    // }
+    // fetch('/api', options)
   }
 }
