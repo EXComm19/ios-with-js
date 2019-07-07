d38ce3771bb7b0e8287423fca7075ef9cf4cdc6f
EXComm19 <excomm19@icloud.com>
2019-07-07 17:53:12 +1000

    Fix cant return home and make code more organised

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
-
-// app.post('/api', (req, res) => {
-//   const data = req.body
-//   fs.writeFile('C:\\note.txt', data.note, (err) => {
-//     if (err) throw err;
-//   });
-//   res.json() = data
-// })
--- public/note/note.js
+++ public/note/note.js
@@ -3,18 +3,6 @@ window.onload = function () {
 
   document.getElementById('note').onkeyup = function () {
     const note = document.getElementById('note').value
-    // const data = {
-    //   "note": note,
-    //   "status": "saved"
-    // }
     localStorage.setItem("note", note)
-    // const options = {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(data)
-    // }
-    // fetch('/api', options)
   }
 }
